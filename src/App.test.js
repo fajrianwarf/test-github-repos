3b0870fe5fbc5a130e4b00bf7e907aa7145afbdc
fajrianwarf <fajrianwarfachrul@gmail.com>
2022-05-21 21:30:49 +0700

    add comments and simple styling

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/github public repositories/i);
+  const linkElement = screen.getByText(/github api search repositories/i);
   expect(linkElement).toBeInTheDocument();
 });
